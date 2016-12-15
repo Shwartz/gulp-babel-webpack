76e3b685560db4d61a19680fce11dc4021167c89
Andris Shvarcs <andris.svarcs@yahoo.com>
2016-12-15 07:23:48 +0000

    Working state gulp -> webpack -> dist

---
+++ tmp-js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _Message = require('./modules/Message');
+
+var _Message2 = _interopRequireDefault(_Message);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var msg = new _Message2.default('test');
+
+msg.say('miauu');
+//# sourceMappingURL=app.js.map
